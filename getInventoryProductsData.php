306f0007df64557853cdb4663c8fa65b03230373
kuba <info@weblead.pl>
2022-02-20 11:51:55 +0100

    pierwszy

--- getInventoryProductsData.php
+++ getInventoryProductsData.php
@@ -417,6 +417,7 @@ $response = curl_exec($curl);
                        
                         curl_close($ch);
                         var_dump($ch);
+                        echo 'ch';
                        
             }
         }
