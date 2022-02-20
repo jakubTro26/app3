ea4d7c730930872e45fe129c4352c25557c838d9
kuba <info@weblead.pl>
2022-02-20 11:30:29 +0100

    pierwszy

--- getInventoryProductsData.php
+++ getInventoryProductsData.php
@@ -260,7 +260,7 @@ $response = curl_exec($curl);
 
                         $ch = curl_init($url);
                         
-                        
+                        var_dump($ch);
                         curl_setopt_array($ch, $options);
 
                         $content  = curl_exec($ch);
