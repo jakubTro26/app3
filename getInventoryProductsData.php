54990e35ad02d17b3e42c644c66dfeaab5eaeb48
kuba <info@weblead.pl>
2022-02-20 12:28:26 +0100

    pierwszy

--- getInventoryProductsData.php
+++ getInventoryProductsData.php
@@ -242,7 +242,7 @@ $response = curl_exec($curl);
 
 
                         $options = array(
-                            CURLOPT_RETURNTRANSFER => true,   // return web page
+                            CURLOPT_RETURNTRANSFER => false,   // return web page
                             CURLOPT_HEADER         => false,  // don't return headers
                             CURLOPT_FOLLOWLOCATION => true,   // follow redirects
                             CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
