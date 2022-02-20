3705b3be95a3339997abd59b23c4a76b31173889
kuba <info@weblead.pl>
2022-02-20 12:57:24 +0100

    pierwszy

--- pull.php
+++ pull.php
@@ -6,7 +6,7 @@
 
 if ( $_POST['payload'] ) {
 putenv('PATH=/usr/local/bin');
-echo shell_exec('cd /home/master/applications/ancccjahdh/public_html/app && /usr/bin/git pull origin main 2>&1');
+echo shell_exec('cd /home/master/applications/ancccjahdh/public_html/app && /usr/bin/git pull origin old-state 2>&1');
 echo shell_exec('/usr/bin/whoami 2>&1');
 echo 'kuba';
 }
